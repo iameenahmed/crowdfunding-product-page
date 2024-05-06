a144d2a792537b13a74d9ea6083cd6db239aa0b5
Ameen A <abulhashim351@gmail.com>
2024-05-06 21:06:25 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Commissioner: ["Commissioner", "sans-serif"],
+      },
+      colors: {
+        ModerateCyan: "hsl(176, 50%, 47%)",
+        DarkCyan: "hsl(176, 72%, 28%)",
+        DarkGray: "hsl(0, 0%, 48%)",
+      },
+    },
+  },
+  plugins: [],
+};
