99830299d6b1ac952c1df1bf4b76c2edc6efd809
Ameen A <abulhashim351@gmail.com>
2024-05-08 09:36:46 +0500

    Add Hero component for displaying hero images

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import hero from "../assets/image-hero-mobile.jpg";
+import heroDesktop from "../assets/image-hero-desktop.jpg";
+
+const Hero = () => {
+  return (
+    <picture className="relative top-0">
+      <source srcSet={heroDesktop} media="(min-width: 768px)" />
+      <img
+        src={hero}
+        alt="iMac desktop computer and a magic keyboard on a desk"
+      />
+    </picture>
+  );
+};
+export default Hero;
